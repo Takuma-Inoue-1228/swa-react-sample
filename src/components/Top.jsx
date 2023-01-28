088e9f317fd314aa017f310d7b1a317586f4ce53
TakumaInoue <inouetakuma19921228@gmail.com>
2023-01-28 13:59:15 +0900

    headerを追加した

--- src/components/Top.jsx
+++ src/components/Top.jsx
@@ -3,8 +3,7 @@ import { Link } from "react-router-dom";
 export const Top = () => {
   return (
     <>
-      <h1 className="text-3xl text-red-500">Hello world!</h1>
-      <h2>Top画面</h2>
+      <h2 className="text-3xl ">Top画面</h2>
       <p>hello world</p>
       <p>
         <Link to={"/about"}>About画面へ</Link>
